a5bedec99b7e7ce228c17faf70dad976f03b7d41
edahanoam <noam.dahan1@mail.huji.ac.il>
2025-04-09 14:52:50 +0300

    the paraphrasing prompts

--- src/axis_augmentation/paraphrase_instruct.py
+++ src/axis_augmentation/paraphrase_instruct.py
@@ -1,4 +1,30 @@
 from base_augmenter import BaseAxisAugmenter
+from typing import List
+
+
+#moran's simplest templates only adding {k times and the return style
+simple_template = (
+    "Please rephrase the following prompt {k} times. "
+    "Return the results as a Python list of strings.\n\n"
+    "'''{prompt}'''"
+)
+
+#moran's gpt3.5 templates, changes to general LLM and {k} times and the
+# return style
+llm_templates = (
+    "Rephrase the following prompt to work better for an LLM. "
+    "Generate {k} different versions and return them as a Python list of strings.\n"
+    "'''{prompt}'''"
+)
+
+Talkative = (
+    "Can you help me write a prompt to an LLM for the following task description? The prompt should be general.\n"
+    "Generate {k} different versions and return them as a Python list of strings.\n"
+    "'''{prompt}'''"
+)
+
+
+
 
 class Paraphrase(BaseAxisAugmenter):
     def __init__(self, k: int = 1):
@@ -11,5 +37,9 @@ class Paraphrase(BaseAxisAugmenter):
         self.k = k
 
 
-    def augment():
+    def augment(self,prompt:str) -> List[str]:
         pass
+
+
+if __name__ == '__main__':
+    print(template)
