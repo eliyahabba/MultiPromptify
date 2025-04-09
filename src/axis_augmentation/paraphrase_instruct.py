0778056f79d02677c9357bba113048606fba2bf2
edahanoam <noam.dahan1@mail.huji.ac.il>
2025-04-09 15:58:22 +0300

    new prompts

--- src/axis_augmentation/paraphrase_instruct.py
+++ src/axis_augmentation/paraphrase_instruct.py
@@ -11,8 +11,8 @@ client = Together()
 #moran's gpt3.5 templates, changes to general LLM and {k} times and the
 # return style
 llm_templates = (
-    "Rephrase the following prompt, providing {k} alternative versions that are better suited for an LLM while preserving the original meaning. Output only a Python list of strings with the alternatives. Do not include any explanation or additional text. \n"
-    "'''Prompt: {prompt}'''"
+    "Rephrase the follbuild_rephrasing_promptowing prompt, providing {k} alternative versions that are better suited for an LLM while preserving the original meaning. Output only a Python list of strings with the alternatives. Do not include any explanation or additional text. \n"
+    "Prompt: '''{prompt}'''"
 )
 
 #moran's begining but adding specifications, restriction on the output and
@@ -20,9 +20,9 @@ llm_templates = (
 talkative = (
     "Can you help me write a prompt to an LLM for the following task "
     "description? Providing {k} creative versions while preserving the "
-    "original meaning. \n Output only a Python list of strings with the "
+    "original meaning. \nOutput only a Python list of strings with the "
     "alternatives. Do not include any explanation or additional text. \n"
-    "'''Prompt: {prompt}'''"
+    "Prompt: '''{prompt}'''"
 )
 prompt_bank =[llm_templates, talkative]
 
@@ -46,9 +46,10 @@ class Paraphrase(BaseAxisAugmenter):
 
 
 def trying_func(k=5):
+    def build_rephrasing_prompt(templete: str, k: int, prompt: str) -> str:
+        return templete.format(k=k, prompt=prompt)
 
-
-
-
+    print(build_rephrasing_prompt(talkative,k,"Given a query word and two other words, determine which of the two words is a homophone of the query word."))
+    prompts =
 if __name__ == '__main__':
-    print(template)
+    trying_func(5)
