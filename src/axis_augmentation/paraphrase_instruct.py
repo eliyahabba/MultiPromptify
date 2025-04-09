c077d8831b2d885ec56d8d00c0f9b5235255e1d0
edahanoam <noam.dahan1@mail.huji.ac.il>
2025-04-09 15:51:22 +0300

    different prompts

--- src/axis_augmentation/paraphrase_instruct.py
+++ src/axis_augmentation/paraphrase_instruct.py
@@ -1,27 +1,31 @@
 from base_augmenter import BaseAxisAugmenter
 from typing import List
+from together import Together
 
 
-#moran's simplest templates only adding {k times and the return style
-simple_template = (
-    "Please rephrase the following prompt {k} times. "
-    "Return the results as a Python list of strings.\n\n"
-    "'''{prompt}'''"
-)
+model = "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"
+client = Together()
+
+
 
 #moran's gpt3.5 templates, changes to general LLM and {k} times and the
 # return style
 llm_templates = (
-    "Rephrase the following prompt to work better for an LLM. "
-    "Generate {k} different versions and return them as a Python list of strings.\n"
-    "'''{prompt}'''"
+    "Rephrase the following prompt, providing {k} alternative versions that are better suited for an LLM while preserving the original meaning. Output only a Python list of strings with the alternatives. Do not include any explanation or additional text. \n"
+    "'''Prompt: {prompt}'''"
 )
 
-Talkative = (
-    "Can you help me write a prompt to an LLM for the following task description? The prompt should be general.\n"
-    "Generate {k} different versions and return them as a Python list of strings.\n"
-    "'''{prompt}'''"
+#moran's begining but adding specifications, restriction on the output and
+# the word "creative"
+talkative = (
+    "Can you help me write a prompt to an LLM for the following task "
+    "description? Providing {k} creative versions while preserving the "
+    "original meaning. \n Output only a Python list of strings with the "
+    "alternatives. Do not include any explanation or additional text. \n"
+    "'''Prompt: {prompt}'''"
 )
+prompt_bank =[llm_templates, talkative]
+
 
 
 
@@ -39,6 +43,11 @@ class Paraphrase(BaseAxisAugmenter):
 
     def augment(self,prompt:str) -> List[str]:
         pass
+
+
+def trying_func(k=5):
+
+
 
 
 if __name__ == '__main__':
